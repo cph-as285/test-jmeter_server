675f506093deb2111ab8ce51316d157d17cf9f76
Alexander Steen <ale.steen@hotmail.com>
2017-03-15 14:23:07 +0100

    Added simple

---
+++ jmeter_server/Controllers/BankController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace jmeter_server.Controllers
+{
+    public class BankController : ApiController
+    {
+        [System.Web.Http.HttpPost]
+        public HttpResponseMessage Interest()
+        {
+            return Request.CreateResponse("Hallo");
+        }
+    }
+}
